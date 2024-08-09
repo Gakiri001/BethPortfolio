64b71bda47707e93f7d7bc8b77f08c0ff9b43f88
Gakiri001 <gakirijulius@gmail.com>
2024-08-09 22:09:47 +0300

    gallery2

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -1,6 +1,9 @@
 import image1 from "../assets/pic33.jpg"
 import image2 from "../assets/pic2.jpg"
 import image3 from "../assets/pic3.jpg"
+import image4 from "../assets/pic4.jpg"
+import image5 from "../assets/pic5.jpg"
+import image6 from "../assets/pic6.jpg"
 
 const gallery = [
   {
@@ -17,6 +20,21 @@ const gallery = [
     image: image3,
     qoute: "Simplicity is the ultimate sophistication.",
     sub:"True beauty lies in simplicity and grace.",
+  },
+  {
+    image: image4,
+    qoute: "Nature's beauty is a silent poem",
+    sub:"The natural world is silently poetic.",
+  },
+  {
+    image: image5,
+    qoute: "A joyful heart makes the face beautiful.",
+    sub:"Happiness enhances external beauty.",
+  },
+  {
+    image: image6,
+    qoute: "She is the strength of generations",
+    sub:"Her power shapes the future.",
   },
 ];
 
