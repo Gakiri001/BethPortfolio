0c58b1171214b5ae950cb176a501d4498ba51639
Gakiri001 <gakirijulius@gmail.com>
2024-08-12 17:04:43 +0300

    accordion responsiveness

--- src/Pages/Blog/Bts.jsx
+++ src/Pages/Blog/Bts.jsx
@@ -1,22 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import bts from "../../data/bts";
 import "./Blog.css";
 
-const BtsCard = ({ BTSQuestion, BTSAnswer }) => {
+const BtsCard = ({ BTSQuestion, BTSAnswer, i, selected, toggle }) => {
   return (
     <div className="BtsCard">
       <div className="item">
-        <div className="title">
-          <h2>{BTSQuestion}</h2>
-          <span> + </span>
+        <div className="title" onClick={() => toggle(i)}>
+          <h3>{BTSQuestion}</h3>
+          <span> {selected === i ? "-" : "+"} </span>
         </div>
-        <div>{BTSAnswer}</div>
+        <div className={selected === i ? "content show" : "content"}>
+          {BTSAnswer}
+        </div>
       </div>
     </div>
   );
 };
 
 function Bts() {
+  const [selected, setSelected] = useState(null);
+
+  const toggle = (i) => {
+    if (selected == i) {
+      return setSelected(null);
+    }
+    setSelected(i);
+  };
   return (
     <div className="BlogWrapper">
       <h1>Behind-the-Scenes Experiences</h1>
@@ -25,6 +35,9 @@ function Bts() {
           key={i}
           BTSQuestion={current.BTSQuestion}
           BTSAnswer={current.BTSAnswer}
+          i={i}
+          selected={selected}
+          toggle={toggle}
         />
       ))}
     </div>
--- src/Pages/Blog/Fashion.jsx
+++ src/Pages/Blog/Fashion.jsx
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import fashion from "../../data/fashion";
 import "./Blog.css";
 
-const FashionCard = ({ FashionQuestion, FashionAnswer }) => {
+const FashionCard = ({
+  FashionQuestion,
+  FashionAnswer,
+  i,
+  selected,
+  toggle,
+}) => {
   return (
     <div className="FashionCard">
       <div className="item">
-        <div className="title">
-          <h2>{FashionQuestion}</h2>
-          <span> + </span>
+        <div className="title" onClick={() => toggle(i)}>
+          <h3>{FashionQuestion}</h3>
+          <span> {selected === i ? "-" : "+"} </span>
         </div>
-        <div>{FashionAnswer}</div>
+        <div className={selected === i ? "content show" : "content"}>
+          {FashionAnswer}
+        </div>
       </div>
     </div>
   );
 };
 
 function Fashion() {
+  const [selected, setSelected] = useState(null);
+
+  const toggle = (i) => {
+    if (selected == i) {
+      return setSelected(null);
+    }
+    setSelected(i);
+  };
+
   return (
     <div className="BlogWrapper">
       <h1>Fashion and Style Advice</h1>
@@ -25,6 +42,9 @@ function Fashion() {
           key={i}
           FashionQuestion={current.FashionQuestion}
           FashionAnswer={current.FashionAnswer}
+          i={i}
+          selected={selected}
+          toggle={toggle}
         />
       ))}
     </div>
